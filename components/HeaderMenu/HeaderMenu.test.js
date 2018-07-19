745a5804a170650667254d9b44dcd2e788f3c225
Andres Carreño <hello@andrescarreno.co>
2018-07-19 01:24:36 -0500

    HeaderMenu test

--- components/HeaderMenu/HeaderMenu.test.js
+++ components/HeaderMenu/HeaderMenu.test.js
@@ -1,3 +1,19 @@
-test("Tests setup to work", () => {
-  expect(true).toBe(true);
+import React from "react";
+import Enzyme, { shallow } from "enzyme";
+import toJson from "enzyme-to-json";
+import Adapter from "enzyme-adapter-react-16";
+
+import HeaderMenu from "./HeaderMenu";
+
+Enzyme.configure({ adapter: new Adapter() });
+
+describe("HeaderMenu", () => {
+  it("renders correctly", () => {
+    const props = {
+      title: "",
+      items: [],
+    };
+    const wrapper = shallow(<HeaderMenu {...props} />);
+    expect(toJson(wrapper)).toMatchSnapshot();
+  });
 });
--- jest.config.js
+++ jest.config.js
@@ -65,8 +65,12 @@ module.exports = {
   //   "node"
   // ],
 
+  modulePaths: ["stubs"],
+
   // A map from regular expressions to module names that allow to stub out resources with a single module
-  // moduleNameMapper: {},
+  moduleNameMapper: {
+    "^.*\\.scss$": "SCSSStub.js",
+  },
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
   // modulePathIgnorePatterns: [],
