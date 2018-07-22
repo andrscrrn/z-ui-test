9d623d9037343867bde273e4a3d78a24230f6077
Andres Carreño <hello@andrescarreno.co>
2018-07-22 13:05:12 -0500

    Adding Hero base styles

--- components/Hero/Hero.js
+++ components/Hero/Hero.js
@@ -3,5 +3,15 @@ import React from "react";
 import "./Hero.scss";
 
 export default function Hero() {
-  return <div className="hero" />;
+  return (
+    <div className="Hero">
+      <div className="Hero__content">
+        <div className="Hero__opinion-box">
+          <div className="Hero__opinion-box-content" />
+          <div className="Hero__opinion-box-buttons" />
+        </div>
+      </div>
+      <div className="Hero__counter">Counter</div>
+    </div>
+  );
 }
