731df6c3b3c77283b8f112cad18219e5460b962e
Andres Carreño <hello@andrescarreno.co>
2018-07-24 22:19:19 -0500

    Adding blank pages and fixing responsive issues

---
+++ pages/how-it-works.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "../styles/main.scss";
+
+const Page = () => (
+  <div className="container">
+    <h1>How It Works</h1>
+  </div>
+);
+
+export default Page;
---
+++ pages/past-trials.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "../styles/main.scss";
+
+const Page = () => (
+  <div className="container">
+    <h1>Past Trials</h1>
+  </div>
+);
+
+export default Page;
